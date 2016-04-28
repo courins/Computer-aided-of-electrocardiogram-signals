45de7992535829580a6b0c2a26f98d4a0b13ad1b
kirimati <ibalex.salino@gmail.com>
2016-04-28 12:28:16 +0200

    fdshdfghdfgh

--- Normal_ECG_Signals.m
+++ Normal_ECG_Signals.m
@@ -19,47 +19,51 @@ ecg3 = ecg;
 
 subplot(1,3,1);
 plot(ecg1);
-xlabel('time');
-ylabel('ecg signal');
-title('normal ecg1');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
+title('Normal ECG1');
 
 subplot(1,3,2);
 plot(ecg2);
-xlabel('time');
-ylabel('ecg signal');
-title('normal ecg2');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
+title('Normal ECG2');
 
 subplot(1,3,3);
 plot(ecg3);
-xlabel('time');
-ylabel('ecg signal');
-title('normal ecg3');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
+title('Normal ECG3');
 
 
 % display signals on 4 seconds
-tmin = 1;
+tmin = 3;
 tmax = 5;
 [ecg_w1, t_w1] =  windowECG(ecg1, fs1, tmin, tmax);
 [ecg_w2, t_w2] =  windowECG(ecg2, fs2, tmin, tmax);
-[ecg_w3, t_w3] =  windowECG(ecg2, fs3, tmin, tmax);
+[ecg_w3, t_w3] =  windowECG(ecg3, fs3, tmin, tmax);
 
 figure;
-subplot(1,3,1);
+% subplot(1,3,1);
 plot(t_w1, ecg_w1);
-xlabel('time');
-ylabel('ecg signal');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
 title('normal ecg 1');
 
-subplot(1,3,2);
+
+
+figure;
+% subplot(1,3,2);
 plot(t_w2, ecg_w2);
-xlabel('time');
-ylabel('ecg signal');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
 title('normal ecg 2');
 
-subplot(1,3,3);
+figure;
+% subplot(1,3,3);
 plot(t_w3, ecg_w3);
-xlabel('time');
-ylabel('ecg signal');
+xlabel('Time(s)');
+ylabel('Voltage(microV)');
 title('normal ecg 3');
 
 
